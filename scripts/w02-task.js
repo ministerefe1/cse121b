599b7f9da80bdb003041c2df57e2d169425760a9
ministerefe1 <efeoghenemaro201@gmail.com>
2024-01-23 13:00:52 +0100

    modified w02-task.js

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -2,25 +2,73 @@
 
 /* Step 1 - Setup type tasks - no code required */
 
-
 /* Step 2 - Variables */
-const fullName = `Efe Enahoro`;
+const fullName = "Efe Enahoro";
 const currentYear = new Date().getFullYear();
-const profilePicture = `images/efe1.jpg`;
-
+const profilePicture = "images/efe1.jpg";
 
 /* Step 3 - Element Variables */
-const nameElement = document.querySelector("#name");
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const foodElement = document.querySelector("#food");
-
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.textContent = fullName;
 yearElement.textContent = currentYear;
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}. He is currently a student of BYUI`);
 
 /* Step 5 - Array */
-let favoriteFood = ["Beans", "Rice", "Cereals", "Chicken", "Ukodo", "Cassava", "Yam"];
-foodElement.innerHTML = "<br>" + favoriteFood.join("<br>");
+let favoriteFood = ["Beans", " Rice", " Cereals", " Chicken", " Ukodo", " Cassava", " Yam"];
+document.getElementById("food").innerHTML = favoriteFood;
+
+
+
+// // function gpaponits(grade) {
+// //     let gpaponits = 0;
+// // }
+// // switch (grade) {
+// //     case "A":
+// //         gpaponits = 4;
+// //         break;
+// //     case "B":
+// //         gpaponits = 3;
+// //         break;
+// //     case "C":
+// //         gpaponits = 2;
+// //         break;
+// //     default:
+// //         gpaponits = -1;
+// //     return gpaponits;
+// // }
+
+// // console.log(gpaponits("A"))
+// // console.log(gpaponits("B"))
+// // console.log(gpaponits("C"))
+// // console.log(gpaponits("X"))
+
+
+// // //Alternatively =
+// // function gpaponits(grade) {
+// //     let gpaponits = 0;
+// //     if (grade==="A") {
+// //         gpaponits = 4;
+// //     }
+// //     else if (grade==="B") {
+// //         gpaponits = 3;
+// //     }
+// //     else if (grade==="C") {
+// //         gpaponits = 2;
+// //     }
+// //     else {
+// //         gpaponits = -1;
+// //     }
+// //     return gpaponits;
+// // }
+// // console.log(gpaponits("A"))
+// // console.log(gpaponits("B"))
+// // console.log(gpaponits("C"))
+// // console.log(gpaponits("X"))
+
 
